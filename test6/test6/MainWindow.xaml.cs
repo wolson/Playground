dfa70c0734a55b618f216dcc993e1ff7abcb3247
wolson <walter.olson@mercmarine.com>
2016-01-15 07:59:49 -0600

    Initial commit.

---
+++ test6/test6/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace test6
+{
+    public partial class MainWindow : ContentPage
+    {
+        private string MainText { get; set; }
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            MainText = "Welcome to Test VI";
+        }
+    }
+}
